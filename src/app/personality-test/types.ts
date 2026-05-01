4f6f38365cd5cb22fa9585bfdfd0e3a1751c3762
zhangshn <364109505@qq.com>
2026-05-01 11:34:01 +0800

    Add personality-test local types

---
+++ src/app/personality-test/types.ts
@@ -0,0 +1,9 @@
+/**
+ * 当前文件负责：定义 personality-test 页面局部使用的类型。
+ */
+
+import type { ZiweiFlowType } from "../../ai/ziwei-core/dynamic/dynamic-schema"
+
+export type DynamicGenderInput = "" | "male" | "female"
+
+export type ActiveDynamicFlow = ZiweiFlowType
