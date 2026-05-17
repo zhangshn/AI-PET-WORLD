bfa94074ff171ecb3ac66838eb648b14b73e3fab
zhangshn <364109505@qq.com>
2026-05-17 22:34:51 +0800

    Use world creation runtime from core world layer

--- src/app/world/page.tsx
+++ src/app/world/page.tsx
@@ -10,7 +10,7 @@ import { useRouter } from "next/navigation"
 import {
   CREATE_WORLD_STORAGE_KEY,
   parseCreateWorldInput,
-} from "./world-creation-runtime"
+} from "@/world/creation/world-creation-runtime"
 
 import styles from "./page.module.css"
 
@@ -18,9 +18,7 @@ export default function WorldPage() {
   const router = useRouter()
 
   useEffect(() => {
-    const rawCreateWorldInput = window.localStorage.getItem(
-      CREATE_WORLD_STORAGE_KEY
-    )
+    const rawCreateWorldInput = window.localStorage.getItem(CREATE_WORLD_STORAGE_KEY)
     const createWorldInput = parseCreateWorldInput(rawCreateWorldInput)
 
     if (!createWorldInput) {
