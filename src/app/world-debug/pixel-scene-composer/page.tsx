fa67bf7490afec87d9a735653a06c3382a99cd52
zhangshn <364109505@qq.com>
2026-05-26 09:07:31 +0800

    Add pixel scene composer debug page

---
+++ src/app/world-debug/pixel-scene-composer/page.tsx
@@ -0,0 +1,7 @@
+// 该页面用于承载像素世界组合算法测试组件。
+
+import PixelSceneComposerClient from "./pixel-scene-composer-client";
+
+export default function PixelSceneComposerPage() {
+  return <PixelSceneComposerClient />;
+}
