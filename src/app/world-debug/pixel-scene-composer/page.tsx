5880a5c9e806a850479d3671c3a7923969decc31
zhangshn <364109505@qq.com>
2026-05-30 01:44:42 +0800

    refactor: remove old composer visual debug route

--- src/app/world-debug/pixel-scene-composer/page.tsx
+++
@@ -1,7 +0,0 @@
-// 该页面用于通过合并后的像素视觉 Debug 实验室打开组合预览视图。
-
-import PixelVisualLabClient from "../pixel-visual-lab/pixel-visual-lab-client";
-
-export default function PixelSceneComposerPage() {
-  return <PixelVisualLabClient initialMode="composer" />;
-}
