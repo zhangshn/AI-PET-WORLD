3c2903399be289bc2feb059062f58cace86e977b
zhangshn <364109505@qq.com>
2026-05-31 11:09:20 +0800

    feat: export pixel primitives module

---
+++ src/world/pixel-primitives/index.ts
@@ -0,0 +1,11 @@
+// 该文件用于统一导出像素原型库能力。
+
+export * from "./pixel-primitive-schema";
+export * from "./pixel-style-foundation";
+export * from "./semantic-structure-library";
+export * from "./pixel-primitive-library";
+export * from "./pixel-shape-library";
+export * from "./pixel-part-library";
+export * from "./pixel-object-recipes";
+export * from "./pixel-object-validator";
+export * from "./pixel-primitive-svg-renderer";
