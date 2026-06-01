816bfbd283a9b426879de5885933e4c0a1415a2a
zhangshn <364109505@qq.com>
2026-06-01 15:28:12 +0800

    feat: add pixel art recipes public exports

---
+++ src/world/pixel-art-recipes/index.ts
@@ -0,0 +1,13 @@
+// 该文件用于统一导出程序化像素美术生成公共能力。
+
+export * from "./core/color-utils";
+export * from "./core/grid-utils";
+export * from "./core/mask-utils";
+export * from "./core/pixel-block-builder";
+export * from "./core/quantize-grid";
+export * from "./core/seeded-noise";
+
+export * from "./filters/contact-shadow-filter";
+export * from "./filters/environment-blend-filter";
+export * from "./filters/shape-noise-filter";
+export * from "./filters/texture-dither-filter";
